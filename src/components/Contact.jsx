ebf6e603e4cece5344cd9b06ec7bdbecba6c0b38
CYNTHIA MATAYO <flexx684@gmail.com>
2024-09-28 09:18:26 +0300

    contact update and fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,64 +4,50 @@ import img from "../assets/images/contact-img.svg";
 const Contact = () => {
   return (
     <div id="contact" className="max-w-[1040px] m-auto md:pl-20 p-6 py-10">
-      <h1 className="py-1 text-4xl font-bold text-center text-[#001b5e]">
-        Contact
-      </h1>
-      <div className="wrapper flex justify-center py-16 gap-52 lg:py-12">
-        <div className="hidden lg:block">
+      <div className="wrapper flex justify-center  py-16 gap-52 lg:py-12">
+        <div className=" hidden lg:block">
           <img className="w-[550px] h-full" src={img} alt="" />
         </div>
-        <div className="w-full lg:w-[600px]"> {/* Adjust width of the form container */}
+        <div className="content-box">
+          <h1 className="text-black font-bold text-4xl">Get In Touch</h1>
           <form
             action="https://getform.io/f/fa4f1c9d-0dee-4c6e-b3b6-3386a33d786a"
             method="POST"
             encType="multipart/form-data"
-            className="bg-white p-8 rounded-lg shadow-lg"
+            className="flex flex-col "
           >
-            <div className="grid md:grid-cols-2 gap-4 w-full py-2">
-              <div className="flex flex-col">
-                <label className="uppercase text-sm py-1">Name</label>
-                <input
-                  className="border-2 rounded-lg p-2 flex border-gray-300"
-                  type="text"
-                  name="name"
-                />
-              </div>
-              <div className="flex flex-col">
-                <label className="uppercase text-sm py-1">Phone Number</label>
-                <input
-                  className="border-2 rounded-lg p-2 flex border-gray-300"
-                  type="text"
-                  name="phone"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col py-1">
-              <label className="uppercase text-sm py-1">Email</label>
+            <div className="flex gap-4 py-2">
               <input
-                className="border-2 rounded-lg p-2 flex border-gray-300"
-                type="email"
-                name="email"
-              />
-            </div>
-            <div className="flex flex-col py-2">
-              <label className="uppercase text-sm py-1">Subject</label>
-              <input
-                className="border-2 rounded-lg p-2 flex border-gray-300"
+                className="bg-transparent h-14 rounded-2xl  border-[1px] border-black pl-4 text-black placeholder-black lg:w-36 "
                 type="text"
-                name="subject"
+                placeholder="Name"
+              />
+              <input
+                className="bg-transparent h-14  rounded-2xl  border-[1px] border-black pl-4 text-black placeholder-black  lg:w-36"
+                type="text"
+                placeholder="Last Name"
               />
             </div>
-            <div className="flex flex-col py-2">
-              <label className="uppercase text-sm py-1">Message</label>
-              <textarea
-                className="border-2 rounded-lg p-2 flex border-gray-300"
-                rows="5"
-                name="message"
-              ></textarea>
+            <div className="flex gap-4  py-4  ">
+              <input
+                className="bg-transparent h-14  rounded-2xl   border-[1px] border-black pl-4 text-black placeholder-black  lg:w-36"
+                type="email"
+                placeholder="Email"
+              />
+              <input
+                className="bg-transparent h-14  rounded-2xl  border-[1px] border-black pl-4 text-black placeholder-black  lg:w-36"
+                type="phone"
+                placeholder="Phone"
+              />
             </div>
-            <button className="bg-[#001b5e] text-gray-100 mt-4 w-full p-4 rounded-lg">
-              Send Message
+            <textarea
+              className="bg-transparent rounded-2xl border-[1px] border-black h-36 pl-4 text-black w-full placeholder-black pt-2"
+              rows={40}
+              cols={35}
+              placeholder="Message"
+            ></textarea>
+            <button className="bg-[#001b5e] border-black  text-gray-100 border-[1] py-[16px] rounded-lg px-8 my-8 w-32 font-bold ">
+              Send
             </button>
           </form>
         </div>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -9,10 +9,9 @@ const Project = () => {
   return (
     <div id='project' className='max-w-[1040px] m-auto md:pl-20 p-4 py-8'>
         <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Projects</h1>
-        <p className='text-center py-8'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-             Sequi officiis aut, quasi, voluptatibus iste dolorem delectus, 
-             esse assumenda unde alias id laborum necessitatibus
-             minima vero quidem eligendi nobis dolore voluptatem.</p>
+        <p className='text-center py-8'>In this Project I applied my skills for codding and software development as a whole.
+        The process is document to show the life cycle of the project.
+        </p>
         <div className='grid sm:grid-cols-2 gap-12'>
             <ProjectItem img={codeImg} title= 'Code App'/>
             <ProjectItem img={mobileImg} title= 'Moble App'/>
